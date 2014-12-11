93045c15b4ad841a1744f712048fd9d67cfeadf7
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2014-12-11 15:59:19 +0100

    gruntfile finished 1

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+console.log('\'Allo \'Allo!');
+
+var player1 = {name: "Chris", score: 1000, rank: 1};
+var player2 = {name: "Kristofer", score: 100000, rank: 2};
+
+function playerDetails(){
+console.log("The name of the player is "+ this.name + "."+ " His score is : "+ this.score + "and his rank : "+ this.rank);
+}
+player1.logDetails= playerDetails;
+player2.logDetails= playerDetails;
+
+player1.logDetails();
+player2.logDetails();
